ca7dbd75129a8cfb5164a0b5677d733a044580f4
hernandezluisalejandro09@gmail.com <hernandezluisalejandro09@gmail.com>
2024-02-09 12:03:06 -0400

    progreso

---
+++ javascriptdepagina.js
@@ -0,0 +1,42 @@
+
+    let variable1 = "hola mundo"
+    let variable2 = 4
+    let variable3 = true
+
+    let variable4 = undefined
+    let varibale5 = null
+    let variable6 = [
+        "Manzana",
+        "Arroz",
+        "Arepa",
+        "Maltin Polar",
+        "Pan de Jamón"
+    ]
+
+    let usuario1 = {
+        nombre: "John Doe",
+        lastname: "Diaz Diaz",
+        age: 26,
+        email: "josephdoe@diaz.go",
+        address: {
+            country: "Venezuela", 
+            state: "Carabobo"
+        },
+        children: [
+            "Miguel",
+            "Camila",
+            "José",
+            "Aureliano José",
+            "Aureliano Segundo",
+        ]
+    }
+
+
+console.log(usuario1)
+
+if (false + true){
+    console.log("Se cumplió la condición")
+}
+else {
+    console.log("Si lo de arriba no se cumple")
+}
